bfe2579bd04a39dcee59cc5e26d682e78a160b66
Joao <joaovic2012@hotmail.com>
2023-02-01 12:38:18 -0300

    Increase, decrease

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,49 +1,75 @@
 import './styles.css';
 import * as cartService from '../../../services/cart-service';
 import { OrderDTO, OrderItemDTO } from '../../../models/order';
-import {useState } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 export default function Cart() {
 
     const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
+    function handleIncreaseItem(productId: number) {
+        cartService.increaseItem(productId);
+        setCart(cartService.getCart());
+    }
+
+    function handledDecreaseItem(productId: number) {
+        cartService.decreaseItem(productId);
+        setCart(cartService.getCart());
+    }
+
     return (
         <main>
             <section id="cart-container-section" className="dsc-container">
-                <div className="dsc-card dsc-mb20">
-                    {
-                        cart.items.map(item => (
-                            <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
-                                <div className="dsc-cart-item-left">
-                                    <img src={item.imgUrl} alt={item.name} />
-                                    <div className="dsc-cart-item-description">
-                                        <h3>{item.name}</h3>
-                                        <div className="dsc-cart-item-quantity-container">
-                                            <div className="dsc-cart-item-quantity-btn">-</div>
-                                            <p>{item.quantity}</p>
-                                            <div className="dsc-cart-item-quantity-btn">+</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="dsc-cart-item-right">
-                                    R$ {(item.price * item.quantity).toFixed(2)}
-                                </div>
-                            </div>
-                        ))
-                    }
 
-                    <div className="dsc-cart-total-container">
-                        <h3>R$ 1500.00</h3>
-                    </div>
-                </div>
+                {
+                    cart.items.length === 0
+                        ? (
+                            <div className='dsc-section-title dsc-mb20'>
+                                <h2>Carrinho está vazio</h2>
+                            </div>
+                        )
+                        : (
+                            <div className="dsc-card dsc-mb20">
+                                {
+                                    cart.items.map(item => (
+                                        <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
+                                            <div className="dsc-cart-item-left">
+                                                <img src={item.imgUrl} alt={item.name} />
+                                                <div className="dsc-cart-item-description">
+                                                    <h3>{item.name}</h3>
+                                                    <div className="dsc-cart-item-quantity-container">
+                                                        <div onClick={() =>handledDecreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">-</div>
+                                                        <p>{item.quantity}</p>
+                                                        <div onClick={() =>handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="dsc-cart-item-right">
+                                                R$ {(item.subTotal).toFixed(2)}
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                                <div className="dsc-cart-total-container">
+                                    <h3>R$ {(cart.total).toFixed(2)}</h3>
+                                </div>
+
+                            </div>
+                        )
+
+                }
                 <div className="dsc-btn-page-container">
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
                     </div>
-                    <div className="dsc-btn dsc-btn-white">
-                        Continuar comprando
-                    </div>
+                    <Link to={'/catalog'}>
+                        <div className="dsc-btn dsc-btn-white">
+                            Continuar comprando
+                        </div>
+                    </Link>
+
                 </div>
             </section>
-        </main>
+        </main >
     )
 }
--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -19,3 +19,28 @@ export function addProduct(product :ProductDTO){
         cartRepository.save(cart);
     }
 }
+
+export function clearCart() {
+    return cartRepository.clear;
+}
+
+export function increaseItem(productId :number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find( x => x.productId === productId);
+    if (item) {
+        item.quantity++;
+        cartRepository.save(cart);
+    }
+}
+
+export function decreaseItem(productId :number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find( x => x.productId === productId);
+    if (item) {
+        item.quantity--;
+        if (item.quantity < 1){
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
+        cartRepository.save(cart);
+    }
+}
