62f1000cfbbdca25adcf28688be22596d276124f
Joao <joaovic2012@hotmail.com>
2023-02-16 00:43:08 -0300

    PrivateRouter, hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig } from "axios";
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { requestBackend } from "../utils/request";
 import * as accessTokenRepository from "../LocalStorage/access-token-repository";
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
 
@@ -28,10 +29,40 @@ export function logout() {
     accessTokenRepository.remove();
 }
 
-export function saveAccessToken(token: string){
+export function saveAccessToken(token: string) {
     accessTokenRepository.save(token);
 }
 
-export function getAccessToken(){
+export function getAccessToken() {
     return accessTokenRepository.get();
 }
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = accessTokenRepository.get();
+        return token == null ? undefined : (jwtDecode(token) as AccessTokenPayloadDTO);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+    if (roles.length === 0) {
+    return true;
+    }
+    const tokenPayload = getAccessTokenPayload();
+    if (tokenPayload !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+    if (tokenPayload.authorities.includes(roles[i])) {
+    return true;
+    }
+    }
+    //return roles.some(role => tokenData.authorities.includes(role));
+    }
+    return false;
+    }
