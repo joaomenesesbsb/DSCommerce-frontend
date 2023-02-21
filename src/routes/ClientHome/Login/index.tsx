00a28b78e12ec5450cde695bab7b4e7e09914462
Joao <joaovic2012@hotmail.com>
2023-02-21 11:17:55 -0300

    Custom input

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -4,35 +4,52 @@ import { CredentialsDTO } from '../../../models/auth';
 import * as authService from '../../../services/auth-service';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
+import FormInput from '../../../components/FormInput';
 
 export default function Login() {
 
     const navigate = useNavigate();
 
-    const [formData, setFormData] = useState<CredentialsDTO>({
-        username: '',
-        password: ''
+    const [formData, setFormData] = useState<any>({
+        username: {
+            value: "",
+            id: "username",
+            name: "username",
+            type: "text",
+            placeholder: "Email",
+            validation: function (value: string) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+            },
+            message: "Favor informar um email válido",
+        },
+        password: {
+            value: "",
+            id: "password",
+            name: "password",
+            type: "password",
+            placeholder: "Senha",
+        }
     })
 
-    const {setContextTokenPayload} = useContext(ContextToken);
+    const { setContextTokenPayload } = useContext(ContextToken);
 
     function handleSubmit(e: any) {
         e.preventDefault();
-        authService.loginRequest(formData)
-        .then(response => {
-            authService.saveAccessToken(response.data.access_token); 
-            setContextTokenPayload(authService.getAccessTokenPayload());
-            navigate('/cart');
-        }
+        authService.loginRequest({ username: formData.username.value, password: formData.password.value })
+            .then(response => {
+                authService.saveAccessToken(response.data.access_token);
+                setContextTokenPayload(authService.getAccessTokenPayload());
+                navigate('/cart');
+            }
             )
-            .catch(error => 
+            .catch(error =>
                 console.log(error));
     }
 
     function handleInputChange(e: any) {
         const name = e.target.name;
         const value = e.target.value;
-        setFormData({ ...formData, [name]: value });
+        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
     }
 
     return (
@@ -43,23 +60,17 @@ export default function Login() {
                         <h2>Login</h2>
                         <div className="dsc-form-controls-container">
                             <div>
-                                <input
-                                     name='username'
-                                    value={formData.username}
-                                    className="dsc-form-control "
-                                    type="text"
-                                    placeholder="Email"
+                                <FormInput
+                                    {...formData.username}
+                                    className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
                                 <div className="dsc-form-error"></div>
                             </div>
                             <div>
-                                <input
-                                    name='password'
-                                    value={formData.password}
+                                <FormInput
+                                    {...formData.password}
                                     className="dsc-form-control"
-                                    type="password"
-                                    placeholder="Senha"
                                     onChange={handleInputChange}
                                 />
                             </div>
